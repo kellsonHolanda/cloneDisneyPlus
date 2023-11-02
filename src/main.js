47effdac9079b6d30072e6b6d397e0768392b311
kellsonHolanda <kellson7777@gmail.com>
2023-11-02 11:59:56 -0300

    criação da seção de perguntas frequentes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     const button = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
+    const questions = document.querySelectorAll('[data-faq-question]');
     
     
     for (let i = 0; i < button.length; i++){
@@ -14,8 +15,21 @@ document.addEventListener('DOMContentLoaded', function(){
             
         })
     }
+
+    for (let i=0; i < questions.length; i++){
+        questions[i].addEventListener('click', abrirEFechar);
+    }
 }
 )
+
+function abrirEFechar(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+
+}
+
 function removerBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     for (let i=0; i< buttons.length; i++){
